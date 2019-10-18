d1c1c4a8f2d25008b157e8dd77dcc9b3ea5c1a91
李小明 <likeming@jasgroup.cn>
2019-10-18 18:03:24 +0800

    更换为直接用pubsub发送文件

--- ipfs.js
+++ ipfs.js
@@ -6,75 +6,32 @@ const M = {
   pubsub_peers: null,
   ready: false,
 
-  addFile(files, callback) {
-    const S = this;
-    const fileArray = [];
-    let fileSize = 0;
-    for (const file of files) {
-      let fileReader = new FileReader();
-      fileReader.addEventListener('load', evt => {
-        const buffer = window.Ipfs.Buffer.from(
-            evt.target.result
-        );
-
-        fileArray.push({
-          path: file.name,
-          content: buffer
-        });
-
-        fileSize += buffer.length;
-
-        if (fileArray.length === files.length) {
-          S.node.add(
-              fileArray,
-              {
-                wrapWithDirectory: true,
-                progress: (length) => {
-                  // let percent = (length / FileSize * 100).toFixed(0);
-                  console.debug('文件添加进度:', length, fileSize);
-                }
-              }
-          )
-              .then((results) => {
-                console.debug('文件添加完毕:', results);
-                callback(results);
-              });
-        }
-      });
-      fileReader.readAsArrayBuffer(file);
-    }
-  },
-
   async ui() {
     const S = this;
 
     //消息列表
     const list = document.querySelector('#list');
-    const appendToList = (msg) => {
+    const appendTextToList = (msg) => {
       const json = JSON.parse(msg.data.toString());
       const li = document.createElement('li');
-
-      if (json.ipfs) {
-        li.innerHTML = `
-              <header>${msg.from}</header>
-              <article></article>
-            `;
-        const article = li.querySelector('article');
-
-        for (const fileInfo of json.ipfs) {
-          if (fileInfo.path === '') {
-            continue;
-          }
-
-          const p = document.createElement('p');
-          p.innerHTML = `<a href="https://test.dev.lilu.red/ipfs/${fileInfo.hash}" target="_blank">${fileInfo.path}</a>`;
-          article.append(p);
-        }
-      } else {
-        li.innerHTML = `<header>${msg.from}</header><article><p>${json.text}</p></article>`;
-      }
-
+      li.innerHTML = `<header>${msg.from}</header><article><p>${json.text}</p></article>`;
       list.append(li);
+      //滚动到底部
+      list.parentElement.scrollTop = list.clientHeight;
+    };
+    const appendFileToList = (msg, filename, objectURL) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<header>${msg.from}</header><article><p class="file">${filename}</p></article>`;
+      list.append(li);
+
+      li.querySelector('p.file')
+          .addEventListener('click', () => {
+            let a = document.createElement('a');
+            a.setAttribute('href', objectURL);
+            a.setAttribute('download', filename);
+            a.click();
+          });
+
       //滚动到底部
       list.parentElement.scrollTop = list.clientHeight;
     };
@@ -86,7 +43,7 @@ const M = {
         topic,
         msg => {
           console.info('收到:', msg);
-          appendToList(msg);
+          appendTextToList(msg);
         },
         err => {
           if (err) {
@@ -94,31 +51,108 @@ const M = {
           }
         }
     );
-    // const topic_file = 'test_file';
-    // S.node.pubsub.subscribe(
-    //     topic_file,
-    //     msg => {
-    //       console.info('收到文件:', msg);
-    //
-    //       const objectURL = URL.createObjectURL(
-    //           new Blob([
-    //             window.Ipfs.Buffer.from(msg.data)
-    //           ])
-    //       );
-    //       let a = document.createElement('a');
-    //       a.setAttribute('href', objectURL);
-    //       a.setAttribute('download', 'test.zip');
-    //       a.click();
-    //     },
-    //     err => {
-    //       if (err) {
-    //         console.warn(err);
-    //       }
-    //     }
-    // );
+    const topic_file = 'test_file';
+    S.node.pubsub.subscribe(
+        topic_file,
+        msg => {
+          console.info('收到文件:', msg);
+
+          //分离信息和文件
+          //参考 https://www.npmjs.com/package/buffer#convert-buffer-to-arraybuffer
+          const buffer = window.Ipfs.Buffer.from(msg.data);
+          console.debug('合体:', buffer);
+          const infoBuffer = buffer.buffer.slice(
+              0, 1024
+          );
+          console.debug('信息:', infoBuffer);
+          const fileBuffer = buffer.buffer.slice(
+              1024, buffer.byteLength
+          );
+          console.debug('文件:', fileBuffer);
+
+          //提取有用的部分, 还原文件名称
+          let infoArray = [];
+          let infoView = new Uint8Array(infoBuffer);
+          for (let i = 0; i < infoBuffer.byteLength; i++) {
+            if (infoView[i] > 0) {
+              infoArray.push(infoView[i]);
+            } else {
+              break;
+            }
+          }
+          const info = decodeURI(
+              String.fromCharCode
+                  .apply(null, new Uint8Array(infoArray))
+          );
+          console.debug('信息:', info);
+
+          //创建ObjectURL
+          const objectURL = URL.createObjectURL(
+              new Blob([fileBuffer])
+          );
+
+          appendFileToList(msg, info, objectURL);
+        },
+        err => {
+          if (err) {
+            console.warn(err);
+          }
+        }
+    );
     //发布
+    const publishFiles = (files) => {
+      for (const file of files) {
+        let fileReader = new FileReader();
+        fileReader.addEventListener('load', evt => {
+          console.debug(evt.target.result);
+
+          //字符转ArrayBuffer
+          //参考 https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+          //没有验证 https://coolaj86.com/articles/unicode-string-to-a-utf-8-typed-array-buffer-in-javascript/
+          const str = encodeURI(file.name);
+          let fileInfoBuffer = new ArrayBuffer(1024);
+          let fileInfoView = new Uint8Array(fileInfoBuffer);
+          for (let i = 0, strLen = str.length; i < strLen; i++) {
+            fileInfoView[i] = str.charCodeAt(i);
+          }
+          console.debug(fileInfoBuffer);
+
+          //拼接信息和文件
+          //参考 http://es6.ruanyifeng.com/#docs/arraybuffer#%E6%95%B0%E7%BB%84%E6%96%B9%E6%B3%95
+          function concatenate(resultConstructor, ...arrays) {
+            let totalLength = 0;
+            for (let arr of arrays) {
+              totalLength += arr.length;
+            }
+            let result = new resultConstructor(totalLength);
+            let offset = 0;
+            for (let arr of arrays) {
+              result.set(arr, offset);
+              offset += arr.length;
+            }
+            return result;
+          }
+
+          const bytes = concatenate(Uint8Array, fileInfoView, new Uint8Array(evt.target.result));
+          console.debug(bytes);
+
+          S.node.pubsub.publish(
+              topic_file,
+              window.Ipfs.Buffer.from(
+                  bytes
+              ),
+              err => {
+                if (err) {
+                  console.warn(err);
+                }
+              }
+          );
+        });
+        fileReader.readAsArrayBuffer(file);
+      }
+    };
     const input_text = document.querySelector('#input_text');
-    const button_send = document.querySelector('#button_send');
+    const button_file = document.querySelector('#button_file');
     input_text.addEventListener('dragenter', evt => {
       evt.stopPropagation();
       evt.preventDefault();
@@ -144,29 +178,37 @@ const M = {
       const dataTransfer = evt.dataTransfer;
       const files = dataTransfer.files;
       console.debug('托放文件:', files);
-      // for (const file of files) {
-      //   let fileReader = new FileReader();
-      //   fileReader.addEventListener('load', evt => {
-      //     S.node.pubsub.publish(
-      //         topic_file,
-      //         window.Ipfs.Buffer.from(
-      //             evt.target.result
-      //         ),
-      //         err => {
-      //           if (err) {
-      //             console.warn(err);
-      //           }
-      //         }
-      //     );
-      //   });
-      //   fileReader.readAsArrayBuffer(file);
-      // }
-      S.addFile(files, results => {
+      publishFiles(files);
+    });
+    button_file.addEventListener('click', () => {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.addEventListener('change', () => {
+        publishFiles(
+            input.files
+        );
+      });
+      input.click();
+    });
+    document.addEventListener('keypress', evt => {
+      if (evt.key === 'Enter') {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        const text = input_text.textContent;
+
+        if (text === '') {
+          return;
+        }
+
+        //清空输入框
+        input_text.textContent = '';
+
         S.node.pubsub.publish(
             topic,
             window.Ipfs.Buffer.from(
                 JSON.stringify({
-                  "ipfs": results
+                  text: text
                 })
             ),
             err => {
@@ -175,38 +217,6 @@ const M = {
               }
             }
         );
-      });
-    });
-    button_send.addEventListener('click', () => {
-      const text = input_text.textContent;
-
-      if (text === '') {
-        return;
-      }
-
-      //清空输入框
-      input_text.textContent = '';
-
-      S.node.pubsub.publish(
-          topic,
-          window.Ipfs.Buffer.from(
-              JSON.stringify({
-                text: text
-              })
-          ),
-          err => {
-            if (err) {
-              console.warn(err);
-            }
-          }
-      );
-    });
-    document.addEventListener('keypress', evt => {
-      if (evt.key === 'Enter') {
-        evt.stopPropagation();
-        evt.preventDefault();
-
-        button_send.click();
       }
     });
   },
